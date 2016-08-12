91508f19e72682595b283be9db40775699a3485b
Vladislav Veluga <vladislav805@yandex.com>
2016-08-13 00:30:59 +0300

    fix language data editor: added pagination (issuse #4)

--- lang/editor.php
+++ lang/editor.php
@@ -28,15 +28,18 @@
 	};
 
 	if (isset($_REQUEST["save"])) {
-var_dump($_POST);
-print "<br><br><br><br><br><br><br><br><br>";
-		$json = json_encode($_POST, JSON_UNESCAPED_UNICODE);
-var_dump($json);
-exit;
-		$fh = fopen($languageId . ".json", "w+");
-		fwrite($fh, $json);
-		fclose($fh);
-		header("Location: ?languageId=" . $languageId);
+
+		$targetFile = "./" . $languageId . ".json";
+		$data = json_decode(file_get_contents($targetFile), true);
+
+		foreach ($_POST as $section => $v) {
+			foreach ($v as $key => $value) {
+				$data[$section][$key] = $value;
+			};
+		};
+
+		$fh = file_put_contents($targetFile, json_encode($data, JSON_UNESCAPED_UNICODE));
+		header("Location: " .$_SERVER["HTTP_REFERER"]);
 		exit;
 	};
 
@@ -70,22 +73,71 @@ exit;
 		line-height: 28px;
 		padding: 0 8px;
 	}
+
+	.pagination-wrap {
+		border-bottom: 1px solid black;
+		overflow: hidden;
+	}
+
+	.pagination-wrap a {
+		display: block;
+		float: left;
+		text-align: center;
+		line-height: 50px;
+		text-decoration: none;
+		color: black;
+		box-sizing: border-box;
+		border-right: 1px solid black;
+	}
+
+	.pagination-wrap a:last-child {
+		border-right: none;
+	}
+
+	.pagination-wrap a[data-current] {
+		font-weight: 900;
+		color: gray;
+		box-shadow: inset 0 0 6px 1px rgba(0, 0, 0, .5);
+	}
+
+	.pagination-wrap[data-links="1"] a { width: 100%; }
+	.pagination-wrap[data-links="2"] a { width: 50%; }
+	.pagination-wrap[data-links="3"] a { width: 33.3333%; }
+	.pagination-wrap[data-links="4"] a { width: 25%; }
+	.pagination-wrap[data-links="5"] a { width: 20%; }
+	.pagination-wrap[data-links="6"] a { width: 16.6666%; }
+	.pagination-wrap[data-links="7"] a { width: 14.2857%; }
+	.pagination-wrap[data-links="8"] a { width: 12,5%; }
 </style>
 <form action="./editor.php?languageId=<?=$languageId;?>&amp;save=1" method="post" enctype="multipart/form-data">
-<table>
 <?
 	$fields = json_decode(file_get_contents("./0.json"), true);
 	$data = json_decode(file_get_contents("./" . $languageId . ".json"), true);
 
+	$count = 0;
+	foreach ($fields as $a) {
+		foreach ($a as $b) {
+			$count++;
+		};
+	};
+
+
 	$offset = (int) (isset($_REQUEST["offset"]) ? $_REQUEST["offset"] : 0);
-	$perPage = 50;
-	$i = -1;
+	$perPage = 150;
+	$j = -1;
+
+	$pages = pagination($offset, $count, $perPage, 20);
+
+	print $pages;
+
+?><table><?
+
 	foreach ($fields as $section => $s) {
 
 		foreach ($s as $key => $value) {
-			$i++;
+			$j++;
 
-			if (!($i >= $offset && $offset + $perPage < $i)) {
+			if (!($j >= $offset && $offset + $perPage > $j)) {
 				continue 1;
 			};
 
