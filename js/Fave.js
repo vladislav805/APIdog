437e589cb0886a31faa3efd17956353dc78fd9a3
Vladislav Veluga <vladislav805@yandex.com>
2016-08-22 02:06:05 +0300

    adding photos, videos; some fixes

--- js/Fave.js
+++ js/Fave.js
@@ -8,23 +8,18 @@
 var Fave = {
 
 	RequestPage: function() {
-
-		switch (Site.get("section")) {
+		var section;
+		switch (section = Site.get("section")) {
 			case "links":
-				return Fave.page(Fave.links);
-
-
 			case "posts":
-				return Fave.page(Fave.posts);
-
-
-			case "photos":  return Fave.Photos(offset);break;
-			case "videos":  return Fave.Videos(offset);break;
-
+			case "photos":
+			case "videos":
 			case "users":
+				return Fave.page(Fave[section]);
+
 			default:
 				return Fave.page(Fave.users);
-		}
+		};
 	},
 
 	getTabs: function () {
@@ -102,17 +97,19 @@ var Fave = {
 			};
 		};
 
-		if (!controller.hasCache()) {
-			isLoading = true;
-			controller.loadNext(reset);
-		} else {
-			controller.reset();
-			controller.show(Fave.MAX_INSERTING);
-		};
-
 		controller.onMetaData = function(count, all) {
 			header.innerHTML = lg(controller.lang.header).schema({ n: count, f: lg(controller.lang.items) });
 			isAll = all;
+		};
+
+		if (!controller.hasCache()) {
+			isLoading = true;
+			controller.reset();
+			controller.loadNext(reset);
+		} else {
+			controller.cursor = 0;
+			controller.onMetaData(controller.cache.length, true);
+			controller.show(Fave.MAX_INSERTING);
 		};
 
 		Site.append(wrap).setHeader(lg("fave.title"));
@@ -178,8 +175,8 @@ f = fields
 		},
 
 		show: function(max) {
-			for (var i = 0, k = this.cursor; i < max && this.cache[i]; ++i, ++this.cursor)  {
-				this.nodes.list.appendChild(Templates.getUser(this.cache[i], { fulllink: true, actions: (
+			for (var i = 0, k = this.cursor; i < max && this.cache[k]; ++i, ++k, ++this.cursor)  {
+				this.nodes.list.appendChild(Templates.getUser(this.cache[k], { fulllink: true, actions: (
 					$.e("div", {"class": "i i24 fr settings-i-remove", onclick: function(event) {
 						event.preventDefault();
 
@@ -233,24 +230,34 @@ f = fields
 		},
 
 		show: function(max) {
-			console.log(this.cursor);
-			var w;
-			for (var i = 0, k = this.cursor; i < max && this.cache[i]; ++i, ++this.cursor)  {
-				w = this.cache[i];
-				console.log(w);
-				this.nodes.list.appendChild(Templates.getUser({
-					screen_name: w.url.split("/")[3],
-					name: w.title,
-					photo_50: w.photo_50,
-					photo_100: w.photo_100
-				}, { fulllink: true, actions: (
-					$.e("div", {"class": "i i24 fr settings-i-remove", onclick: function(event) {
-						event.preventDefault();
+			var w, self = this;
+			for (var i = 0, k = this.cursor; i < max && this.cache[k]; ++i, ++k, ++this.cursor)  {
+				w = this.cache[k];
+				(function(list, item) {
 
-						return false;
-					}})
-				)}));
+					list.appendChild(Templates.getUser({
+						screen_name: item.url.split("/")[3],
+						name: item.title,
+						photo_50: item.photo_50,
+						photo_100: item.photo_100
+					}, { fulllink: true, actions: [
+						$.e("div", {"class": "i i24 fr settings-i-remove", onclick: function(event) {
+							event.preventDefault();
+							self.remove(item.id, null);
+							return false;
+						}})
+					]}));
+
+				})(this.nodes.list, w);
 			}
+		},
+
+		add: function(url, callback) {
+
+		},
+
+		remove: function(id, callback) {
+
 		}
 	},
 
@@ -311,21 +318,13 @@ return false;
 */
 
 	posts: {
-
 		cursor: 0,
-
 		cache: [],
-
 		lang: { header: "fave.headTitle", items: "fave.postsPosts" },
-
 		nodes: { form: null, list: null },
-
 		reset: function() { this.cursor = 0; this.cache = []; },
-
 		hasCache: function() { return false; },
-
 		getNodeSearchForm: function() { return null; },
-
 		getNodeList: function() { return this.nodes.list = $.e("div"); },
 
 		loadNext: function(reset) {
@@ -360,55 +359,86 @@ return false;
 
 	},
 
+	photos: {
+		cursor: 0,
+		cache: [],
+		lang: { header: "fave.headTitle", items: "fave.photosPhotos" },
+		nodes: { form: null, list: null },
+		reset: function() { this.cursor = 0; this.cache = []; },
+		hasCache: function() { return false; },
+		getNodeSearchForm: function() { return null; },
+		getNodeList: function() { return this.nodes.list = $.e("div"); },
 
-	Photos: function (offset) {
-		Site.APIv5("fave.getPhotos", {
-			count: 40,
-			offset: offset,
-			extended: 1,
-			v: 5.29
-		}, function (data) {
-			data = Site.isResponse(data);
-			var parent = document.createElement("div"),
-				list = document.createElement("div"),
-				count = data.count;
-			data = data.items;
-			parent.appendChild(Fave.GetTabs());
-			parent.appendChild(Site.CreateHeader("У Вас в закладках " + count + " " + $.TextCase(count, ["фотография","фотографии","фотографий"])));
-			if (!count)
-				parent.appendChild(Site.EmptyField("Ничего нет.."));
-			else
-				for (var i = 0, l = data.length; i < l; ++i) {
-					data[i].likes = {count: 0, user_likes: 0};
-					data[i].comments = {count: 0};
-					list.appendChild(Photos.itemPhoto(data[i]));
-				};
-			parent.appendChild(list);
-			parent.appendChild(Site.PagebarV2(Site.Get("offset"), count, 40));
-			Site.SetHeader("Закладки");
-			Site.Append(parent);
-		});
+		loadNext: function(reset) {
+			var self = this;
+			console.log(this.cursor);
+			new APIRequest("fave.getPhotos", {
+				count: Fave.MAX_INSERTING,
+				offset: this.cursor,
+				v: 5.52,
+				extended: 1
+			})
+				.setWrapper(APIDOG_REQUEST_WRAPPER_V5)
+				.setOnCompleteListener(function(res) {
+
+					self.onMetaData(res.count, res.count <= self.cache.length);
+					reset();
+					self.cache = self.cache.concat(res.items);
+					self.show(Fave.MAX_INSERTING);
+				})
+				.execute();
+		},
+
+		show: function(max) {
+			var w;
+			for (var i = 0, k = this.cursor; i < max && this.cache[k]; ++i, ++k, ++this.cursor)  {
+				w = this.cache[k];
+				w.likes = {count: 0, user_likes: 0};
+				w.comments = {count: 0};
+				this.nodes.list.appendChild(Photos.itemPhoto(w));
+			};
+		}
+
 	},
-	Videos: function (offset) {
-		Site.API("fave.getVideos", {
-			count: 30,
-			offset: offset,
-			extended: 1,
-			need_likes: 1
-		},function(data){
-			data = Site.isResponse(data);
-			var parent = document.createElement("div"),
-				list = document.createElement("div");
-			parent.appendChild(Fave.GetTabs());
-			parent.appendChild(Site.CreateHeader("У Вас в закладках " + data[0] + " " + $.TextCase(data[0], ["видеозапись","видеозаписи","видеозаписей"])));
-			if (data[0] == 0)
-				parent.appendChild($.elements.create("div",{"class": "msg-empty", html: "Ничего нет.."}))
-			for(var i = 1; i < data.length; ++i)
-				list.appendChild(Video.item(Video.tov5(data[i])));
-			parent.appendChild(list);
-			parent.appendChild(Site.PagebarV2(Site.Get("offset"),data[0],40));
-			Site.SetHeader("Закладки");
-			Site.Append(parent);
-		});
+
+	videos: {
+		cursor: 0,
+		cache: [],
+		lang: { header: "fave.headTitle", items: "fave.videosVideos" },
+		nodes: { form: null, list: null },
+		reset: function() { this.cursor = 0; this.cache = []; },
+		hasCache: function() { return false; },
+		getNodeSearchForm: function() { return null; },
+		getNodeList: function() { return this.nodes.list = $.e("div"); },
+
+		loadNext: function(reset) {
+			var self = this;
+			new APIRequest("fave.getVideos", {
+				count: Fave.MAX_INSERTING,
+				offset: this.cursor,
+				v: 5.52,
+				extended: 1
+			})
+				.setWrapper(APIDOG_REQUEST_WRAPPER_V5)
+				.setOnCompleteListener(function(res) {
+					Local.add(res.profiles);
+					Local.add(res.groups);
+
+					self.onMetaData(res.count, res.count <= self.cache.length);
+					reset();
+					self.cache = self.cache.concat(res.items);
+					self.show(Fave.MAX_INSERTING);
+				})
+				.execute();
+		},
+
+		show: function(max) {
+			var w;
+			for (var i = 0, k = this.cursor; i < max && this.cache[k]; ++i, ++k, ++this.cursor)  {
+				w = this.cache[k];
+				this.nodes.list.appendChild(Video.item(w));
+			};
+		}
+
 	}
 };
