16383376f80a3904ffee11a66020ebde51a66bb8
Vladislav Veluga <vladislav805@yandex.com>
2016-09-01 00:04:48 +0300

    issue #61: 12/35

--- js/Wiki.js
+++ js/Wiki.js
@@ -6,7 +6,7 @@
 
 var Wiki={checkEvent:function(e){return ((e=(e||window.event))&&(e.type=='click'||e.type=='mousedown'||e.type=='mouseup')&&(e.which>1||e.button>1||e.ctrlKey||e.shiftKey||browser.mac&&e.metaKey))||false},switchHider:function(el){var box=el.parentNode.parentNode;if($.elements.hasClass(box,"wk_hider_box"))box.className=box.className.replace('wk_hider_box','wk_hider_box_opened');else box.className=box.className.replace('wk_hider_box_opened','wk_hider_box')},
 	WIKI_STYLES: "//apidog.ru/includes/wiki.css",
-	parseContent: function (text) {
+	parseContent: function(text) {
 		return text
 			.replace(/onclick="return showVideoAjax\('-?\d+_\d+', this\);"/img, "")
 			.replace(/\/images\/play_video.png/img, "\/\/vk.com\/images\/play_video.png")
@@ -15,24 +15,23 @@ var Wiki={checkEvent:function(e){return ((e=(e||window.event))&&(e.type=='click'
 			})
 			.replace(/\n/mg, "<br \/>");
 	},
-	insertStyles: function () {
+	insertStyles: function() {
 		var e = document.getElementsByTagName("link");
-		for (var i = 0, l = e.length; i < l; ++i)
-			if (e[i].link == Wiki.WIKI_STYLES)
+		for (var i = 0, l = e.length; i < l; ++i) {
+			if (e[i].link === Wiki.WIKI_STYLES) {
 				return;
+			};
+		};
 
-		document.getElementsByTagName("head")[0].appendChild($.e("link", {rel: "stylesheet", href: Notes.WIKI_STYLES}));
+		getHead().appendChild($.e("link", {rel: "stylesheet", href: Notes.WIKI_STYLES}));
 	},
-	initNormalize: function ()
-	{
+	initNormalize: function() {
 		var videos = document.querySelectorAll(".wk_video"),
 			docs = document.querySelectorAll(".wk_doc a"),
-			getThumbnailVideo = function (link)
-			{
+			getThumbnailVideo = function(link) {
 				return link.querySelector("img");
 			};
-		Array.prototype.forEach.call(videos, function (node)
-		{
+		Array.prototype.forEach.call(videos, function(node) {
 			var video = {}, t = getThumbnailVideo(node), id = node.href.split("#")[1].replace(/video(-?\d+)_(\d+)/img, "$1 $2 ").split(" ");
 			video.owner_id = id[0];
 			video.id = id[1];
@@ -42,10 +41,8 @@ var Wiki={checkEvent:function(e){return ((e=(e||window.event))&&(e.type=='click'
 			node.parentNode.insertBefore(Video.getAttachment(video), node);
 			$.elements.remove(node);
 		});
-		console.log(docs);
-		Array.prototype.forEach.call(docs, function (node)
-		{
-			console.log(node, node.href.split("#")[1]);
+		Array.prototype.forEach.call(docs, function(node) {
+//			console.log(node, node.href.split("#")[1]);
 			node.href = "\/\/vk.com\/" + node.href.split("#")[1];
 		});
 	}
