49ce69aa14942d0692291731452989cbef6c9384
Vladislav Veluga <vladislav805@yandex.com>
2016-08-31 23:44:33 +0300

    issue #61: 5/35

--- js/Dev.js
+++ js/Dev.js
@@ -7,44 +7,51 @@
 var Dev = {
 	chate: {},
 	inited: false,
-	init: function () {
-		document.getElementsByTagName("head")[0].appendChild($.e("link", {rel: "stylesheet", href: "/includes/dev.css"}));
+
+	init: function() {
+		getHead().appendChild($.e("link", {rel: "stylesheet", href: "/includes/dev.css"}));
 		Dev.inited = true;
 	},
-	explain: function (url) {
+
+	explain: function(url) {
 		url = !url ? "methods" : url;
-		if (Dev.chate[url])
+
+		if (Dev.chate[url]) {
 			return Dev.show(Dev.chate[url]);
-		if (!Dev.inited)
+		};
+
+		if (!Dev.inited) {
 			Dev.init();
+		};
+
 		Site.Loader();
-		Support.Ajax("/api/v2/apidog.getVKDevPage", {
+
+		APIdogRequest("apidog.getVKDevPage", {
 			p: url
-		}, function (data) {
+		}, function(data) {
 			data = data.response;
 			Dev.chate[data.page] = data;
 			Dev.show(data);
 		})
 	},
-	adaptive: function (html) {
+	adaptive: function(html) {
 		return html.replace(/<a([A-Za-z0-9"'_\s-]+)href="\/dev\//img, "<a$1href=\"#dev/");
 	},
-	show: function (page) {
+	show: function(page) {
 		var e = $.e,
 			w = e("div", {"class": "dev-wrap"});
 
-		w.appendChild(Site.CreateHeader(page.title));
+		w.appendChild(Site.getPageHeader(page.title));
 		w.appendChild(e("div", {html: Dev.adaptive(page.html)}));
 
-		Site.Append(w);
-		Site.SetHeader(page.title);
+		Site.append(w);
+		Site.setHeader(page.title);
 	}
 };
+
 var Analyzes = {
-	open: function (name, ownerId)
-	{
-		includeScripts(["/utilites.js"], function (event)
-		{
+	open: function(name, ownerId) {
+		includeScripts(["/utilites.js"], function(event) {
 			Analyzes.open(name, ownerId);
 		});
 	}
