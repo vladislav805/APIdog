4466a15eae60701b88a80b3055f64e034872694a
Vladislav Veluga <vladislav805@yandex.com>
2016-09-27 00:02:42 +0300

    #61

--- js/Snapster.js
+++ js/Snapster.js
@@ -15,11 +15,11 @@ function SnapsterItem (d) {
 
 SnapsterItem.prototype = {
 
-	getId: function () {
+	getId: function() {
 		return this.ownerId + "_" + this.postId;
 	},
 
-	getNode: function () {
+	getNode: function() {
 		return $.e("div", {"class": "snapster-item", append: [
 			this._getHeader($.e),
 			this._getContent($.e),
@@ -27,29 +27,29 @@ SnapsterItem.prototype = {
 		]});
 	},
 
-	getOwner: function () {
+	getOwner: function() {
 		return Local.Users[this.ownerId];
 	},
 
-	getLinkForOwner: function () {
+	getLinkForOwner: function() {
 		return "#" + this.getOwner().screen_name;
 	},
 
-	getLink: function () {
+	getLink: function() {
 		return "#chronicle?act=item&id=" + this.getId();
 	},
 
-	getDate: function () {
+	getDate: function() {
 		var d = new Date(this.date * 1000);
 
 		return d.getHours().fix00() + ":" + d.getMinutes().fix00();
 	},
 
-	getComments: function () {
+	getComments: function() {
 		return this.photo.getComments();
 	},
 
-	_getHeader: function (e) {
+	_getHeader: function(e) {
 		return e("div", {"class": "snapster-head", append: [
 			e("a", {
 				"class": "snapster-head-time",
@@ -69,25 +69,25 @@ SnapsterItem.prototype = {
 		]});
 	},
 
-	_getContent: function (e) {
+	_getContent: function(e) {
 		var start = Date.now() / 1000, self = this;
 		return e("div", {"class": "snapster-content", append: [
-			e("img", {src: self.photo.getURL("x"), onload: function (event) {
+			e("img", {src: self.photo.getURL("x"), onload: function(event) {
 				if (self.photo.getURL("y") && Date.now() / 1000 - start < 2)
 					this.src = self.photo.getURL("y");
 			}})
 		]});
 	},
 
-	_getFooter: function (e) {
+	_getFooter: function(e) {
 		var comments = this.getComments(),
 			self = this,
 			actions = e("div", {"class": "snapster-footer-actions", append: [
 				this._fac = e("div", {"class": "fr snapster-footer-actions-comment"}),
-				this._fal = e("div", {"class": "fl snapster-footer-actions-like", onclick: function (event) {
+				this._fal = e("div", {"class": "fl snapster-footer-actions-like", onclick: function(event) {
 					self.like();
 				}}),
-				this._falt = e("div", {"class": "fl snapster-footer-actions-likes", html: this._getLikesText(), onclick: function (event) {
+				this._falt = e("div", {"class": "fl snapster-footer-actions-likes", html: this._getLikesText(), onclick: function(event) {
 					self.showLikes();
 				}}),
 			]}),
@@ -96,23 +96,23 @@ SnapsterItem.prototype = {
 		return wrap;
 	},
 
-	_getCommentNode: function (e, comments) {
-		var node = e("div", {"class": "snapster-footer-comments", append: comments.items.map(function (i) {
+	_getCommentNode: function(e, comments) {
+		var node = e("div", {"class": "snapster-footer-comments", append: comments.items.map(function(i) {
 			return i.getNode();
 		})});
 		return node;
 	},
 
-	_getLikesText: function (p) {
+	_getLikesText: function(p) {
 		p = p || this.photo.likes;
 		return p ? "Нравится " + p + " " + $.textCase(p, ["человеку", "людям", "людям"]) : "Никому не понравилось";
 	},
 
-	like: function (callback) {
+	like: function(callback) {
 		var self = this;
 		Site.API("execute", {
 			code: 'var p={type:"photo",item_id:' + this.photo.photoId + ',owner_id:' + this.photo.ownerId + ',access_key:"' + this.photo.accessKey + '"},me=API.likes.isLiked(p),act;act=me==0?API.likes.add(p):API.likes.delete(p);return[(-me)+1,act.likes];'
-		}, function (result) {
+		}, function(result) {
 			data = Site.isResponse(result);
 			var l = data[1], i = data[0];
 			if (callback) {
@@ -125,7 +125,7 @@ SnapsterItem.prototype = {
 		});
 	},
 
-	showLikes: function () {
+	showLikes: function() {
 		likers("photo", this.photo.ownerId, this.photo.photoId, this.photo.accessKey);
 	}
 };
@@ -150,19 +150,19 @@ function SnapsterPhoto (d) {
 	this.photos = this._parseImages(d.sizes);
 };
 SnapsterPhoto.prototype = {
-	_parseImages: function (sizes) {
+	_parseImages: function(sizes) {
 		var d = {};
-		sizes.forEach(function (size) {
+		sizes.forEach(function(size) {
 			d[size.type] = size;
 		});
 		return d;
 	},
-	getURL: function (sizeId) {
+	getURL: function(sizeId) {
 		return this.photos[sizeId].src;
 	},
-	getComments: function () {
+	getComments: function() {
 		var c = this.comments || {items: [], count: 0}, self = this;
-		c.items = c.items.map(function (q) {
+		c.items = c.items.map(function(q) {
 			return new SnapsterComment(q, self);
 		});
 		return c;
@@ -176,11 +176,11 @@ function SnapsterComment (d, parent) {
 	this.parent = parent;
 };
 SnapsterComment.prototype = {
-	getAuthor: function () {
+	getAuthor: function() {
 		return Local.Users[this.fromId];
 	},
 
-	getNode: function () {
+	getNode: function() {
 		var e = $.e, a = this.getAuthor();
 		return e("div", {"class": "snapster-shortComment-item", append: [
 			e("a", {href: "#" + a.screen_name, html: getName(a)}),
@@ -189,11 +189,11 @@ SnapsterComment.prototype = {
 		]});
 	},
 
-	deleteComment: function (callback) {
+	deleteComment: function(callback) {
 		Site.API("photos.deleteComment", {
 			owner_id: this.parent.ownerId,
 			comment_id: this.commentId
-		}, function (result) {
+		}, function(result) {
 			result = Site.isResponse(result);
 			if (callback) {
 				callback(result);
@@ -204,7 +204,7 @@ SnapsterComment.prototype = {
 
 var Snapster = {
 
-	resolve: function (act) {
+	resolve: function(act) {
 		switch (act) {
 			case "item":
 				var id = Site.get("id");
@@ -226,7 +226,7 @@ var Snapster = {
 
 	feed: {
 
-		open: function () {
+		open: function() {
 			var wrap = $.e("div");
 			wrap.appendChild(Site.getPageHeader("Snapster", $.e("a", {"class": "fr", href: "#chronicle?act=upload", html: "Загрузить"})));
 			wrap.appendChild($.e("div", {append: Site.Loader(true), id: Snapster.feed.wrapElementId}));
@@ -236,13 +236,13 @@ var Snapster = {
 
 		wrapElementId: "snapsterWrap",
 
-		loadItems: function (startFrom, callback) {
+		loadItems: function(startFrom, callback) {
 			Site.APIv5("chronicle.getFeed", {
 				count: 30,
 				fields: "photo_100,online,screen_name",
 				start_from: startFrom || "",
 				v: 5.38
-			}, function (data) {
+			}, function(data) {
 				data = Site.isResponse(data);
 				if (!data)
 					return;
@@ -254,10 +254,10 @@ var Snapster = {
 			});
 		},
 
-		showItems: function (data) {
+		showItems: function(data) {
 			var wrap = $.element(Snapster.feed.wrapElementId);
 
-			data.forEach(function (item) {
+			data.forEach(function(item) {
 				wrap.appendChild(item.getNode({isFeed: true}));
 			});
 
@@ -266,36 +266,36 @@ var Snapster = {
 	},
 
 	upload: {
-		show: function () {
+		show: function() {
 
 		}
 	},
 
 	item: {
-		show: function () {
+		show: function() {
 
 		}
 	},
 
 	search: {
-		show: function () {
+		show: function() {
 
 		}
 	},
 
 	profile: {
-		show: function () {
+		show: function() {
 
 		}
 	},
 
-	parse: function (d) {
+	parse: function(d) {
 		return Array.isArray(d)
-			? d.map(function (o) { return new SnapsterItem(o) })
+			? d.map(function(o) { return new SnapsterItem(o) })
 			: new SnapsterItem(d);
 	},
 
-	RequestPage: function () {
+	RequestPage: function() {
 		Snapster.resolve(Site.get("act"));
 	}
 };
