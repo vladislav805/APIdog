d7397682a1ba21364fd6fc153492ae158df05cc3
Vladislav Veluga <vladislav805@yandex.com>
2017-09-23 14:21:31 +0300

    6.5 death

--- lib/assembler.php
+++
@@ -1,8 +0,0 @@
-<?
-	$dir = scandir("../js/");
-	header("Content-type: text/javascript; charset=utf-8");
-	foreach ($dir as $file) {
-		if (strpos($file, "js")) {
-			print file_get_contents("../js/" . $file);
-		};
-	};
