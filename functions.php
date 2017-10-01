904636bc318e4662680a6898f46699b552e884fb
Vladislav Veluga <vladislav805@yandex.com>
2017-10-01 12:05:12 +0300

    Change no. build

--- functions.php
+++ functions.php
@@ -18,7 +18,7 @@
 	define("MONTH", 30 * DAY);
 
 	define("APIDOG_VERSION", "6.4.6");
-	define("APIDOG_BUILD", "20170930");
+	define("APIDOG_BUILD", "20171001");
 
 	/**
 	 * Return param from query string/post body by key
