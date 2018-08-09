566b22ca4471eb6a38de54144405be4296e1a3ce
Vladislav Veluga <vladislav805@yandex.com>
2018-08-10 01:11:42 +0300

    Change domain on YandexMetrika

--- js/functions.js
+++ js/functions.js
@@ -815,7 +815,7 @@ function initStatLiveInternet() {
 }
 
 function initStatYandexMetrika() {
-	(function (d,w,c){(w[c]=w[c]||[]).push(function(){try{w.yaCounter19029880=new Ya.Metrika({id:19029880,trackHash:!0})}catch(e){}});var n=d.getElementsByTagName("script")[0],s=d.createElement("script"),f=function(){n.parentNode.insertBefore(s,n);};s.type="text/javascript";s.async=!0;s.src="https://d31j93rd8oukbv.cloudfront.net/metrika/watch_ua.js";if(w.opera==="[object Opera]")d.addEventListener("DOMContentLoaded",f,!1);else f()})(document,window,"yandex_metrika_callbacks");
+	(function (d,w,c){(w[c]=w[c]||[]).push(function(){try{w.yaCounter19029880=new Ya.Metrika({id:19029880,trackHash:!0})}catch(e){}});var n=d.getElementsByTagName("script")[0],s=d.createElement("script"),f=function(){n.parentNode.insertBefore(s,n);};s.type="text/javascript";s.async=!0;s.src="https://cdn.jsdelivr.net/npm/yandex-metrica-watch/watch.js";if(w.opera==="[object Opera]")d.addEventListener("DOMContentLoaded",f,!1);else f()})(document,window,"yandex_metrika_callbacks");
 }
 
 function initStatGoogleAnalytics() {
